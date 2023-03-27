2662adda4c80659b9cd3b08a1285541edc55ff9e
César Greppin <Citcatjoe@users.noreply.github.com>
2023-03-27 15:29:55 +0200

    avancé

--- js/main.js
+++ js/main.js
@@ -1,29 +1,20 @@
 (function($){
 
-	// data = [
-    //     { 
-    //         title: 'titre 1',
-    //         chapeau: 'chapeau 1'
-    //     },
-    //     { 
-    //         title: 'titre 2',
-    //         chapeau: 'chapeau 2'
-    //     }	
-	// ];
-	// console.log(data);
+	
 
-    // buildCards(data);
-    // function buildCards(data){
-    //     var source   = $("#card-template").html();
-    //     var template = Handlebars.compile(source);
-    //     var html = template({'card':data});
-    //     $(".authors").prepend(html); 
-    // }
-
-    var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 111}});
-    var scene;
-    var scene2;
+    var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 200}});
+    
     var array = [];
+
+    $( ".sw-link" ).click(function() {
+        var link = $(this).attr('sw-link');
+        var slider = link.slice(1, 2);
+        var page =  link.slice(3, 4);
+        alert('swiper' + slider + '.slideTo(' + page + ')');
+
+        eval('swiper' + slider + '.slideTo(' + page + ')');
+        
+      });
 
     setTimeout(function() { 
         $('body').addClass('is-visible');
@@ -53,9 +44,10 @@
 
     const swiper1 = new Swiper('.swiper1', {
         direction: 'vertical',
-        loop: true,
+        loop: false,
         effect: "fade",
         speed: 200,
+        allowTouchMove: false,
         navigation: {
           nextEl: '.apres1',
           prevEl: '.avant1',
@@ -65,29 +57,20 @@
     
 
     function setScene(){
+        //var array = [];
         switch (swiper0.activeIndex) { 
             case 0: 
                 console.log('Loop of creation comes...');
                 $( ".swiper-slide-active *[data-sm]").each(function( index ) {
                     var dataSm = $(this).attr('data-sm');
                     
-                    //window[dataSm]();
                     
                     array[index] = new ScrollMagic.Scene({triggerElement: this})
                     .on("enter", function (event) {
                         eval(dataSm + "()");
-                        //alert('EVENT SLIDE '+swiper0.activeIndex+' '+dataSm);
                     }).triggerHook(0.8).addIndicators().addTo(controller);
                 });
-                
-                // array[0] = new ScrollMagic.Scene({triggerElement: ".s0t1"})
-                // .on("enter", function (event) {
-                //     alert('EVENT SLIDE 0');
-                // }).triggerHook(0.8).addIndicators().addTo(controller);
-                // array[1] = new ScrollMagic.Scene({triggerElement: ".s0t2"})
-                // .on("enter", function (event) {
-                //     alert('EVENT SLIDE 0');
-                // }).triggerHook(0.8).addIndicators().addTo(controller);
+               
                 break;
             case 1: 
                 console.log('Loop of creation comes...');
@@ -102,22 +85,13 @@
                         //alert('EVENT SLIDE '+swiper0.activeIndex+' '+dataSm);
                     }).triggerHook(0.8).addIndicators().addTo(controller);
                 });
-                // console.log('Loading slide 1 scene');
-                // array[0] = new ScrollMagic.Scene({triggerElement: ".s1t1"})
-                // .on("enter", function (event) {
-                //     alert('EVENT SLIDE 1');
-                // }).triggerHook(0.8).addIndicators().addTo(controller);
-                // array[1] = new ScrollMagic.Scene({triggerElement: ".s1t1"})
-                // .on("enter", function (event) {
-                //     alert('EVENT SLIDE 1');
-                // }).triggerHook(0.8).addIndicators().addTo(controller);
+               
                 break;
             case 2: 
                 console.log('Loop of creation comes...');
                 $( ".swiper-slide-active *[data-sm]").each(function( index ) {
                     var dataSm = $(this).attr('data-sm');
                     
-                    //window[dataSm]();
                     
                     array[index] = new ScrollMagic.Scene({triggerElement: this})
                     .on("enter", function (event) {
@@ -125,15 +99,7 @@
                         //alert('EVENT SLIDE '+swiper0.activeIndex+' '+dataSm);
                     }).triggerHook(0.8).addIndicators().addTo(controller);
                 });
-                // console.log('Loading slide 2 scene');
-                // array[0] = new ScrollMagic.Scene({triggerElement: ".s2t1"})
-                // .on("enter", function (event) {
-                //     alert('EVENT SLIDE 2');
-                // }).triggerHook(0.8).addIndicators().addTo(controller);
-                // array[1] = new ScrollMagic.Scene({triggerElement: ".s2t1"})
-                // .on("enter", function (event) {
-                //     alert('EVENT SLIDE 2');
-                // }).triggerHook(0.8).addIndicators().addTo(controller);
+               
                 break;
         }
     
@@ -144,7 +110,7 @@
         console.log('Loop of destruction comes...')
         for (var i=0; i<array.length; i++) {
             array[i].destroy(true);
-            array[i] = null;
+            //array[i] = null;
         }
         
         
@@ -159,6 +125,7 @@
     // STORYTELLING FUNCTIONS
     function actionA(){
         alert('CODE FONCTION A');
+        
     }
 
     function actionB(){
@@ -166,11 +133,13 @@
     }
 
     function actionC(){
-        alert('CODE FONCTION C');
+      
+        swiper1.slideTo(2);
     }
 
     function actionD(){
-        alert('CODE FONCTION D');
+        //alert('CODE FONCTION D');
+        swiper1.slideTo(3);
     }
 
     function actionE(){
